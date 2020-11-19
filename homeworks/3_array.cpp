750feb2f5009495ff7a6632de9b250772c90ca38
Mauro Pelucchi <mauro.pelucchi@gmail.com>
2020-11-19 08:23:29 +0100

    add 3 homework

---
+++ homeworks/3_array.cpp
@@ -0,0 +1,16 @@
+
+/*
+Scrivere un programma (con funzioni!) per gestire un array di numeri interi.
+
+In particolare il problema si occupa di generare un array di 50 numeri casuali compresi fra 1 e 100 e offrire a menu le seguenti scelte:
+1. Calcolare la media
+2. Visualizzare il massimo
+3. Visualizzare il minimo
+4. Rigenerare l’array
+5. Visualizzare i valori che distano meno di 4 dalla media
+6. Calcolare la moda
+7. Uscire dal programma
+
+nb. per generare numeri casuali usare vedere http://www.cplusplus.com/reference/cstdlib/srand/
+
+*/

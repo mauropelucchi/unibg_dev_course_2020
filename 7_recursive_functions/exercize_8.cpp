fb06f1e409cfd15612d7191c45cf001ebf28ad8b
Mauro Pelucchi <mauro.pelucchi@gmail.com>
2020-12-09 22:12:38 +0100

    update ricerca dicotomica

--- 7_recursive_functions/exercize_8.cpp
+++ 7_recursive_functions/exercize_8.cpp
@@ -9,20 +9,101 @@ l' indice della prima occorrenza nel vettore del valore cercato.
 
 */
 
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+#define N 55
+
+int dicotomica(int v[], int left, int right, int x);
+int cerca(int v[], int len, int x);
+void genera(int a[N]);
+int scegli_numero();
+void bubble_sort(int a[N], int n);
+
+int main()
+{
+  int a[N];
+  int pos = -1;
+  int numero;
+
+  genera(a);
+  bubble_sort(a, N);
+  numero = scegli_numero();
+  pos = cerca(a, N, numero);
+
+  if (pos != -1)
+    cout << "numero trovato in posizione: " << pos << endl;
+  else
+    cout << "numero non trovato";
+
+  return 0;
+}
+
+void genera(int a[N])
+{
+  int i;
+
+  srand(time(NULL));
+  for (i = 0; i < N; i++)
+  {
+    a[i] = rand() % 100 + 1;
+  }
+
+  cout << "Array generato " << endl;
+  for (i = 0; i < N; i++)
+    cout << a[i] << "\t";
+}
+
+int scegli_numero(void)
+{
+  int numero = 0;
+  do
+  {
+    cout << endl;
+    cout << "Scegli un numero da cercare [1, 100]:" << endl;
+    cin >> numero;
+  } while (numero < 1 || numero > 100);
+  return numero;
+}
+
+void bubble_sort(int vet[], int n)
+{
+  int tmp;
+  for (int j = 0; j < n; j++)
+  {
+
+    for (int i = n - 1; i >= j; i--)
+    {
+      if (vet[i] > vet[i + 1])
+      {
+        tmp = vet[i];
+        vet[i] = vet[i + 1];
+        vet[i + 1] = tmp;
+      }
+    }
+  }
+}
+
 int dicotomica(int v[], int left, int right, int x)
 {
   int middle;
 
   if (left > right)
-    return -1
+    return -1;
 
-           middle = (left + right) / 2;
+  middle = (left + right) / 2;
 
   if (x < v[middle])
+  {
     return dicotomica(v, left, middle - 1, x);
+  }
 
   if (x == v[middle])
+  {
     return middle;
+  }
 
   return dicotomica(v, middle + 1, right, x);
 }
@@ -30,4 +111,4 @@ int dicotomica(int v[], int left, int right, int x)
 int cerca(int v[], int len, int x)
 {
   return dicotomica(v, 0, len - 1, x);
-} // end cerca
+}

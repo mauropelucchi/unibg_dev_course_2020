4b18c5b48750e4ab6a492283af575cfce6f5358f
Mauro Pelucchi <mauro.pelucchi@gmail.com>
2020-12-16 23:32:13 +0100

    add 6_binary tree solution

--- homeworks/6_binary_tree.cpp
+++ homeworks/6_binary_tree.cpp
@@ -16,7 +16,7 @@ Example of struct for a node:
 typedef struct node 
 { 
     int data; 
-    struct Node *left, *right; 
+    Node *left, *right; 
 } Node;
 
 Utility function to create a new tree node:
@@ -29,3 +29,62 @@ Node* newNode(int data)
 }
 
 */
+
+
+#include <iostream> 
+using namespace std; 
+   
+typedef struct node 
+{ 
+    int data; 
+    struct node *left, *right; 
+} Node; 
+  
+void print_leaf(Node *root);
+Node* newNode(int data);
+
+
+int main() 
+{ 
+
+    Node *root = newNode(1); 
+    root->left = newNode(2); 
+    root->right = newNode(3); 
+    root->left->left = newNode(4); 
+    root->right->left = newNode(5); 
+    root->right->right = newNode(8); 
+    root->right->left->left = newNode(6); 
+    root->right->left->right = newNode(7); 
+    root->right->right->left = newNode(9); 
+    root->right->right->right = newNode(10); 
+   
+    print_leaf(root); 
+      
+    return 0; 
+} 
+
+void print_leaf(Node *root) 
+{ 
+    if (!root) 
+        return; 
+      
+    if (!root->left && !root->right) 
+    { 
+        cout << root->data << " ";  
+        return; 
+    } 
+  
+    if (root->left) 
+       print_leaf(root->left); 
+          
+    if (root->right) 
+       print_leaf(root->right); 
+}  
+  
+Node* newNode(int data) 
+{ 
+    Node *temp = new Node; 
+    temp->data = data; 
+    temp->left = temp->right = NULL; 
+    return temp; 
+} 
